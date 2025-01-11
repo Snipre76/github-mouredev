fd1153454353a1dd049e379d377e1cea4c4b04bd
Snipre76 <dannycollazos93@gmail.com>
2025-01-11 10:59:18 -0500

    Cambios principales

--- Git/src/git/MiClase.java
+++ Git/src/git/MiClase.java
@@ -4,5 +4,6 @@ package git;
 public class MiClase {
        public static void main(String[] args) {
         System.out.println("¡Hola desde Git y NetBeans!");
+           System.out.println("Danny");
     }
 }
---
+++ Git2/src/git2/Git2.java
@@ -0,0 +1,14 @@
+
+package git2;
+
+
+public class Git2 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Otro ejemplo");
+        
+    }
+    
+}
