28ce34e019347d1e305dcdf81f2f91682662b6fa
Snipre76 <dannycollazos93@gmail.com>
2025-01-12 11:11:05 -0500

    Se actualiza los cambios

--- Git/src/git/Git.java
+++ Git/src/git/Git.java
@@ -6,7 +6,7 @@ public class Git {
 
 
     public static void main(String[] args) {
-
+       
     }
     
 }
--- Git/src/git/MiClase.java
+++ Git/src/git/MiClase.java
@@ -4,6 +4,7 @@ package git;
 public class MiClase {
        public static void main(String[] args) {
         System.out.println("Estoy haciendo un cambio en mi fichero");
-           System.out.println("Danny");
+           System.out.println("New Danny");
+         
     }
 }
