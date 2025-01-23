308f413e84c00646b50d8d822f3c37d703f755a0
Snipre76 <dannycollazos93@gmail.com>
2025-01-23 12:36:57 -0500

    Correcion fichero

--- Git/src/git/login.java
+++ Git/src/git/login.java
@@ -7,15 +7,9 @@ package git;
 public class login {
    
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println("Login v");
-=======
-        System.out.println("Login v 3");
->>>>>>> login
-=======
+
         System.out.println("Login v_3");
->>>>>>> login
+
     }
      
 }
